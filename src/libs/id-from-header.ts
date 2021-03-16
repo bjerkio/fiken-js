fe2e5e4529b59339287eff981da71a5bc5ea01bc
Simen A. W. Olsen <so@bjerk.io>
2021-03-16 16:32:42 +0100

    wip

--- src/libs/id-from-header.ts
+++ src/libs/id-from-header.ts
@@ -1,28 +1,28 @@
 /**
  * Get ID from `location` header
- * 
+ *
  * This function is used to get the ID resources
  * created in Folio (where you are redirected
  * afterwards).
- * 
+ *
  * @example
  * ```typescript
  * import { getIDFromHeaders } from 'fiken';
- * 
+ *
  * ...
- * 
+ *
  * const res = await fiken.createBankAccountRaw({
  *  ...
  * });
- * 
+ *
  * const id = getIDFromHeaders(res.raw);
  * ```
  */
-export function getIDFromHeaders(resp: Response): string | undefined {
-    if (!resp.headers) return;
-    const loc = resp.headers.get('location');
-    if (!loc) return;
-    
-    const url = loc.split('/');
-    return url.pop();    
+export function getIDFromHeaders(resp: Response): string | undefined {
+  if (!resp.headers) return;
+  const loc = resp.headers.get('location');
+  if (!loc) return;
+
+  const url = loc.split('/');
+  return url.pop();
 }
